7182e5dd463729c3ed89ce605a5f3bbfb2bcee26
Echo <king@email.cn>
2022-10-24 12:40:47 +0800

    改

--- Test/fibonacci.cpp
+++ Test/fibonacci.cpp
@@ -3,32 +3,31 @@
 加入备忘录算法降低时间空间复杂度
 */
 
-
-
 #include <iostream>
 using namespace std;
 int fibona(int);
+int arr[100] = {0};
 int main()
 {
     int n;
-    int array[100] = {0};
     cin >> n;
     cout << fibona(n);
 }
 int fibona(int a)
 {
-    if (array[a]!=0)
+
+    if (arr[a] != 0)
     {
-        /* code */ return array[a];
+        /* code */ return arr[a];
     }
-    
-    if (a== 0||a == 1)
+
+    if (a == 0 || a == 1)
     {
         return 1; /* code */
     }
     else
     {
-        array[a] = fibona(a - 1) + fibona(a - 2);
-        return array[a];
+        arr[a] = fibona(a - 1) + fibona(a - 2);
+        return arr[a];
     }
 }
